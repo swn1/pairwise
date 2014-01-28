705dc7f8ae66b9a02cdc4c405c62d4620bc7d59d
Stephen W. Nuchia <snuchia@gmail.com>
2014-01-28 08:21:51 -0600

    as downloaded http://dl.dropboxusercontent.com/u/8538167/TestFiles/PairWise%20v0_2.zip

---
+++ js/pict.js
@@ -0,0 +1,263 @@
+var PictParser = Editor.Parser = (function() {
+
+  function wordRegexp(words) {
+    return new RegExp("^(?:" + words.join("|") + ")$", "i");
+  }
+
+  var functions = wordRegexp([    
+  ]);
+
+  var keywords = wordRegexp([
+    ":", "and", "or", "in", "not", "xor", "like", "if", "else", "then"
+  ]);
+
+  var types = wordRegexp([
+    "true", "false",
+  ]);
+
+  var operators = wordRegexp([
+        "=", ":=", "<", "<=", "==", "<>", ">", ">=", "like", "in", "xor", "between"
+  ]);
+
+  var operatorChars = /[*+\-<>=&|:\/]/;
+
+  var tokenizePict = (function() {
+    function normal(source, setState) {
+      var ch = source.next();
+      if (ch == "[" || ch == "]" || ch == "{" || ch == "}") {
+        source.nextWhileMatches(/[\w\d]/);
+        return "pict-type";
+    }  
+      else if (ch == "["){
+      setState(inAlias(ch))
+      return "pict-type";
+	  	//return null;
+  }
+      else if (ch == "\"" || ch == "'" || ch == "`") {
+        setState(inLiteral(ch));
+        return null;
+      }
+      else if (ch == "," || ch == ";") {
+        return "pict-separator"
+      }
+      else if (ch == '#') {
+        while (!source.endOfLine()) source.next();
+        return "pict-comment";
+      }
+      else if (ch == '-') {
+        if (source.peek() == "-") {
+          while (!source.endOfLine()) source.next();
+          return "pict-comment";
+        }
+        else if (/\d/.test(source.peek())) {
+          source.nextWhileMatches(/\d/);
+          if (source.peek() == '.') {
+            source.next();
+            source.nextWhileMatches(/\d/);
+          }
+          return "pict-number";
+        }
+        else
+          return "pict-operator";
+      }
+      else if (operatorChars.test(ch)) {
+     
+      if(ch == "/" && source.peek() == "*"){
+        setState(inBlock("pict-comment", "*/"));
+        return null;
+      }
+      
+        else{
+          source.nextWhileMatches(operatorChars);
+          return "pict-operator";
+        }
+        
+      }
+      else if (/\d/.test(ch)) {
+        source.nextWhileMatches(/\d/);
+        if (source.peek() == '.') {
+          source.next();
+          source.nextWhileMatches(/\d/);
+        }
+        return "pict-number";
+      }
+      else if (/[()]/.test(ch)) {
+        return "pict-punctuation";
+      }
+      else {
+        source.nextWhileMatches(/[_\w\d]/);
+        var word = source.get(), type;
+        if (operators.test(word))
+          type = "pict-operator";
+        else if (keywords.test(word))
+          type = "pict-keyword";
+        else if (functions.test(word))
+          type = "pict-function";
+        else if (types.test(word))
+          type = "pict-type";
+        else
+          type = "pict-word";
+        return {style: type, content: word};
+      }
+    }
+
+    function inAlias(quote) {
+	  return function(source, setState) {
+	    while (!source.endOfLine()) {
+		  var ch = source.next();
+		  if (ch == ']') {
+		    setState(normal);
+		    break;
+		  }
+	    }
+	    return "pict-word";
+	  }
+}
+
+function inPrio(quote) {
+    return function(source, setState) {
+        while (!source.endOfLine()) {
+            var ch = source.next();
+            if (ch == ')') {
+                setState(normal);
+                break;
+            }
+        }
+        return "pict-word";
+    }
+}
+
+function inFirstParameter(quote) {
+    return function(source, setState) {
+        while (!source.endOfLine()) {
+            var ch = source.next();
+            if (source.endOfLine()) {
+                ///[\w\d]/
+                setState(normal);
+                break;
+            }
+        }
+        return "pict-var";
+    }
+}
+
+function inNextParameter(quote) {
+    return function(source, setState) {
+        while (!source.endOfLine()) {
+            var ch = source.next();
+            if (ch == ',') {
+                setState(normal);
+                break;
+            }
+        }
+        return "pict-var";
+    }
+}
+
+
+
+    function inLiteral(quote) {
+      return function(source, setState) {
+        var escaped = false;
+        while (!source.endOfLine()) {
+          var ch = source.next();
+          if (ch == quote && !escaped) {
+            setState(normal);
+            break;
+          }
+          escaped = !escaped && ch == "\\";
+        }
+        
+        return quote == "`" ? "pict-quoted-word" : "pict-literal";
+      };
+    }
+
+    function inBlock(style, terminator) {
+      return function(source, setState) {
+        while (!source.endOfLine()) {
+          if (source.lookAhead(terminator, true)) {
+            setState(normal);
+            break;
+          }
+          source.next();
+        }
+        return style;
+      };
+    }
+
+    return function(source, startState) {
+      return tokenizer(source, startState || normal);
+    };
+  })();
+
+  function indentPict(context) {
+    return function(nextChars) {
+      var firstChar = nextChars && nextChars.charAt(0);
+      var closing = context && firstChar == context.type;
+      if (!context)
+        return 0;
+      else if (context.align)
+        return context.col - (closing ? context.width : 0);
+      else
+        return context.indent + (closing ? 0 : indentUnit);
+    }
+  }
+
+  function parsePict(source) {
+    var tokens = tokenizePict(source);
+    var context = null, indent = 0, col = 0;
+    function pushContext(type, width, align) {
+      context = {prev: context, indent: indent, col: col, type: type, width: width, align: align};
+    }
+    function popContext() {
+      context = context.prev;
+    }
+
+    var iter = {
+      next: function() {
+        var token = tokens.next();
+        var type = token.style, content = token.content, width = token.value.length;
+
+        if (content == "\n") {
+          token.indentation = indentPict(context);
+          indent = col = 0;
+          if (context && context.align == null) context.align = false;
+        }
+        else if (type == "whitespace" && col == 0) {
+          indent = width;
+        }
+        else if (!context && type != "pict-comment") {
+          pushContext(";", 0, false);
+        }
+
+        if (content != "\n") col += width;
+
+        if (type == "pict-punctuation") {
+          if (content == "(")
+            pushContext(")", width);
+          else if (content == ")")
+            popContext();
+        }
+        else if (type == "pict-separator" && content == ";" && context && !context.prev) {
+          popContext();
+        }
+
+        return token;
+      },
+
+      copy: function() {
+        var _context = context, _indent = indent, _col = col, _tokenState = tokens.state;
+        return function(source) {
+          tokens = tokenizePict(source, _tokenState);
+          context = _context;
+          indent = _indent;
+          col = _col;
+          return iter;
+        };
+      }
+    };
+    return iter;
+  }
+
+  return {make: parsePict, electricChars: ")"};
+})();
